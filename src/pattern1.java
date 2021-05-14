a445499a3f508f3de2e864970bbf0d3c83a8e5e6
Mridul2804 <om.mridul@gmail.com>
2021-05-14 11:35:50 +0530

    Adding new files

--- src/pattern1.java
+++ src/pattern1.java
@@ -1,6 +1,23 @@
+import java.util.*;
 public class pattern1 {
     public static void main(String[] args){
-        int i, j;
-        
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        char ch=sc.next().charAt(0);
+
+        int size=(n*2)-1;
+        for(int i=1;i<=size;i++){
+            for(int j=1;j<=size;j++){
+
+                if(j==i || j==size-i+1){
+                    System.out.print(ch);
+                }
+                System.out.print(" ");
+
+
+            }
+            System.out.println();
+        }
+
     }
 }
