56f55f434cd2b8b7b0bb6524f1385a44eeb9ee9b
Mridul2804 <om.mridul@gmail.com>
2021-05-14 19:03:35 +0530

    COMMITTING NEW FILES TO MASTER

---
+++ src/Pattern3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pattern3 {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        int temp=1;
+        for(int i=0;i<num;i++){
+            for(int j=0;j<num;j++){
+
+            }
+        }
+    }
+
+}
