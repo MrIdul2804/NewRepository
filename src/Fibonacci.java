2961a6926531b93370bcfcb2cae71d2ebf554818
Mridul2804 <om.mridul@gmail.com>
2021-05-15 14:00:28 +0530

    COMMITTING NEW FILES TO MASTER

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,18 +1,23 @@
 import java.util.*;
 public class Fibonacci {
-    public static void main(String[] args){
-        int n1=0, n2=1;
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1;
         int n;
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        System.out.print(n1+" "+n2);
-        int n3;
-        for(int i=2;i<n;i++){
-            n3=n1+n2;
-            System.out.print(" "+n3);
-            n1=n2;
-            n2=n3;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        if (n < 1) {
+            System.out.println("Invalid Input");
+        } else if (n == 1) {
+            System.out.println(n1);
+        } else {
+            System.out.print(n1 + " " + n2);
+            int n3;
+            for (int i = 2; i < n; i++) {
+                n3 = n1 + n2;
+                System.out.print(" " + n3);
+                n1 = n2;
+                n2 = n3;
+            }
         }
     }
-
 }
